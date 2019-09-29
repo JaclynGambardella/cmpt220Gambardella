3683c05858c05af83ca18e903117d58f38da25e4
Juan Arias <juan.arias@marist.edu>
2019-09-29 14:44:00 -0400

    Feedback

--- labs/4/Lab4Part1.java
+++ labs/4/Lab4Part1.java
@@ -2,19 +2,18 @@ import java.text.DecimalFormat;
 public class Lab4Part1 {
 	public static void main(String[] args) {
 		DecimalFormat dec = new DecimalFormat("0.00");
-			System.out.print("\n|in     cm|cm	  in|");
-			System.out.print("\n---------------------");
-			
-			for (int i=1, j=1; i<=50;){
-				 
-			System.out.printf("\n%2d", i);
+		System.out.print("\n|in     cm|cm	  in|");
+		System.out.print("\n---------------------");
+		
+		for (int i=1, j=1; i<=50; i++, j += 5){
+			System.out.printf("\n%2d  ", i);
 			System.out.print(dec.format(i * 2.54));
 			
-			System.out.print("|%3d",j);
+			System.out.printf(" | %3d  ",j);
 			System.out.print(dec.format(j*0.393701));
-			i = i + 1;
-			j = j + 5;
-			}
+			//i = i + 1;
+			//j = j + 5;
+		}
 	}
 
 }
