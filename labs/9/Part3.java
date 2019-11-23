b80ab99074ca470d5b2aafde1a8fc935ddc92e35
JaclynGambardella <54689062+JaclynGambardella@users.noreply.github.com>
2019-11-22 19:06:06 -0500

    Add files via upload

--- labs/9/Part3.java
+++ labs/9/Part3.java
@@ -1,4 +1,34 @@
+import java.io.*;
+import java.util.Scanner;
+public class Part3{
+	public static void main(String[] args) {
+		if(args.length!=2){
+		System.out.println("Usage: java Part3 <string> <file>");
+		System.exit(0);
 
-public class Part3 {
+		}
 
+try{
+
+Scanner sc = new Scanner(new File(args[1]));
+String lines = "";
+while(sc.hasNextLine()){
+String line = sc.nextLine();
+line = line.replaceAll(args[0], "");
+lines += line+"\n";
+
+}
+
+sc.close();
+PrintWriter print = new PrintWriter(new File(args[1]));
+print.println(lines);
+print.close();
+
+}
+catch(FileNotFoundException e){
+
+System.out.println("Unable to open file");
+
+}
+}
 }
