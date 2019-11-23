72917db9763be30e4934fb8231e2546243c1a340
JaclynGambardella <54689062+JaclynGambardella@users.noreply.github.com>
2019-11-22 19:04:45 -0500

    Add files via upload

--- labs/9/IllegalTriangleException.java
+++ labs/9/IllegalTriangleException.java
@@ -1,4 +1,16 @@
+public class IllegalTriangleException{
 
-public class IllegalTriangleException {
+	public class IllegalTriangleException extends Exception{
+			private double side1;
+			private double side2;
+			private double side3;
+			
+			public IllegalTriangleException(double side1, double side2, double side3){
+				super("The illegal sides are: " + side1 + " " + side2 + " " + side3);
+				this.side1 = side1;
+				this.side2 = side2;
+				this.side3 = side3;
+			}
+	}
 
 }
--- labs/9/Part1.java
+++ labs/9/Part1.java
@@ -1,4 +1,25 @@
 
+import java.util.Scanner; 
+
 public class Part1 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int[] array = new int[100];
+		for(int i = 0; i < array.length; i++) {
+			array[i] = (int)((Math.random() * 100) + 1);
+		}
+		
+		System.out.println("Enter the index of the array: ");
+		int index = input.nextInt();
+		
+		try {
+			System.out.println("The corresponding element value is " + array[index]);
+		}
+		catch(ArrayIndexOutOfBoundsException ex) {
+			System.out.println("Out of bounds");
+		}
+		
+	}
 
 }
